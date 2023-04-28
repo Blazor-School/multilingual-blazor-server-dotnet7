888b0268bc0ed0c0c1140f4197a1daf244d22a18
Red Wei <huy.tran@blazorschool.com>
2023-04-28 14:28:58 +0700

    Add instant translation with URL example.

---
+++ InstantTranslationWithUrl/Utilities/BlazorSchoolRequestCultureProvider.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+using System.Web;
+
+namespace InstantTranslationWithUrl.Utilities;
+
+public class BlazorSchoolRequestCultureProvider : RequestCultureProvider
+{
+    private readonly string _defaultLanguage;
+    private string? _selectedLanguage;
+
+    public BlazorSchoolRequestCultureProvider(string defaultLanguage)
+    {
+        _defaultLanguage = defaultLanguage;
+    }
+
+    public override Task<ProviderCultureResult?> DetermineProviderCultureResult(HttpContext httpContext)
+    {
+        if (httpContext.Request.Headers["Sec-Fetch-Dest"] == "document")
+        {
+            string url = httpContext.Request.GetDisplayUrl();
+            _selectedLanguage = GetLanguageFromUrl(url);
+
+            if (string.IsNullOrEmpty(_selectedLanguage))
+            {
+                _selectedLanguage = _defaultLanguage;
+            }
+
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(_selectedLanguage);
+            CultureInfo.CurrentUICulture = CultureInfo.GetCultureInfo(_selectedLanguage);
+            var result = new ProviderCultureResult(_selectedLanguage);
+
+            return Task.FromResult<ProviderCultureResult?>(result);
+        }
+        else
+        {
+            return Task.FromResult<ProviderCultureResult?>(new ProviderCultureResult(_selectedLanguage));
+        }
+    }
+
+    private string? GetLanguageFromUrl(string url)
+    {
+        var uri = new Uri(url);
+        var urlParameters = HttpUtility.ParseQueryString(uri.Query);
+
+        return urlParameters["language"];
+    }
+}
---
+++ InstantTranslationWithUrl/Utilities/BlazorSchoolStringLocalizer.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+using System.Collections;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+
+namespace InstantTranslationWithUrl.Utilities;
+
+public class BlazorSchoolStringLocalizer<TComponent> : IStringLocalizer<TComponent>
+{
+    private readonly IOptions<LocalizationOptions> _localizationOptions;
+
+    public LocalizedString this[string name] => FindLocalziedString(name);
+    public LocalizedString this[string name, params object[] arguments] => FindLocalziedString(name, arguments);
+
+    public BlazorSchoolStringLocalizer(IOptions<LocalizationOptions> localizationOptions)
+    {
+        _localizationOptions = localizationOptions;
+    }
+
+    public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+    {
+        var resourceManager = CreateResourceManager();
+        var result = new List<LocalizedString>();
+
+        try
+        {
+            var resourceSet = resourceManager.GetResourceSet(CultureInfo.CurrentCulture, true, true);
+            result = resourceSet.Cast<DictionaryEntry>()
+                .Select(item => new LocalizedString((string)item.Key, (string)item.Value, false, GetResourceLocaltion()))
+                .ToList();
+        }
+        catch
+        {
+            result.Add(new("", "", true, GetResourceLocaltion()));
+        }
+
+        return result;
+    }
+
+    private LocalizedString FindLocalziedString(string key, object[]? arguments = default)
+    {
+        var resourceManager = CreateResourceManager();
+        LocalizedString result;
+
+        try
+        {
+            string value = resourceManager.GetString(key);
+
+            if (arguments is not null)
+            {
+                value = string.Format(value, arguments);
+            }
+
+            result = new(key, value, false, GetResourceLocaltion());
+        }
+        catch
+        {
+            result = new(key, "", true, GetResourceLocaltion());
+        }
+
+        return result;
+    }
+
+    private ResourceManager CreateResourceManager()
+    {
+        string resourceLocaltion = GetResourceLocaltion();
+        var resourceManager = new ResourceManager(resourceLocaltion, Assembly.GetExecutingAssembly());
+
+        return resourceManager;
+    }
+
+    private string GetResourceLocaltion()
+    {
+        var componentType = typeof(TComponent);
+        var nameParts = componentType.FullName.Split('.').ToList();
+        nameParts.Insert(1, _localizationOptions.Value.ResourcesPath);
+        string resourceLocaltion = string.Join(".", nameParts);
+
+        return resourceLocaltion;
+    }
+}
